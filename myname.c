b519d9fbdda0decbd8b79f9dc4b509bbd6810ed8
Simanta <simantabawali.pi@gmail.com>
2025-10-20 11:59:20 +0600

    Added student info: Asif Zaman (2410476123)

--- myname.c
+++ myname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Your Name Here\n"); // Replace "Your Name Here" with your actual name
+    printf("Name: Simanta Bawali\n");
+    printf("Student ID: 2410476123\n");
+
     return 0;
 }
