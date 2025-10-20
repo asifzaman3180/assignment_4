8665e4085d1788724fe391359ffe2deea511936e
souravsikderjoy <s2410676135@ru.ac.bd>
2025-10-20 11:57:53 +0600

    Fixed code with proper writing

--- cleancode.c
+++ cleancode.c
@@ -1,3 +1,73 @@
-#include<stdio.h>
-int main(){int a[3][3]={{50,60,70},{80,90,100},{30,40,50}};char n[3][10]={"Ali","Bob","Cat"};int i,j,s;float avg;
-for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}avg=s/3.0;if(avg>=80)printf("%s Grade A avg=%.2f\n",n[i],avg);else if(avg>=60)printf("%s Grade B avg=%.2f\n",n[i],avg);else if(avg>=40)printf("%s Grade C avg=%.2f\n",n[i],avg);else printf("%s Fail avg=%.2f\n",n[i],avg);}int max=0,pos=0;for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}if(s>max){max=s;pos=i;}}printf("Topper: %s with total %d\n",n[pos],max);return 0;}
+#include <stdio.h>
+
+//Function to calculate the average marks of the students
+float calculateAverage(int marks[], int subjectCount) {
+    int total = 0;
+    for (int i = 0; i < subjectCount; i++) {
+        total += marks[i];
+    }
+    return total / (float)subjectCount;
+}
+
+//Function to get the grade of the srudents
+char getGrade(float averageMarks) {
+    if (averageMarks >= 80)
+        return 'A';
+    else if (averageMarks >= 60)
+        return 'B';
+    else if (averageMarks >= 40)
+        return 'C';
+    else
+        return 'F';
+}
+
+int main() {
+    //Name and marks of the students
+    int marks[3][3] = {
+        {50, 60, 70},
+        {80, 90, 100},
+        {30, 40, 50}
+    };
+    char studentNames[3][10] = {"Ali", "Bob", "Cat"};
+
+    int studentIndex, subjectIndex;
+    float averageMarks;
+
+    printf("        STUDENT REPORT CARD SYSTEM       \n");
+    printf("-----------------------------------------\n");
+    printf("%-10s %-10s %-10s\n", "Name", "Average", "Grade");
+    printf("-----------------------------------------\n");
+
+
+    //Loop through each student
+    for (studentIndex = 0; studentIndex < 3; studentIndex++) {
+        //Calculate average and grade
+        averageMarks = calculateAverage(marks[studentIndex], 3);
+        char grade = getGrade(averageMarks);
+
+        //Print eacjh of the student's name, grade and average
+       printf("%-10s %-10.2f %-10c\n", studentNames[studentIndex], averageMarks, grade);
+    }
+
+    //To Find the student with highest marks
+    int highestTotal = 0;
+    int topperIndex = 0;
+
+    for (studentIndex = 0; studentIndex < 3; studentIndex++) {
+        int totalMarks = 0;
+        for (subjectIndex = 0; subjectIndex < 3; subjectIndex++) {
+            totalMarks += marks[studentIndex][subjectIndex];
+        }
+
+        if (totalMarks > highestTotal) {
+            highestTotal = totalMarks;
+            topperIndex = studentIndex;
+        }
+    }
+
+    printf("-----------------------------------------\n");
+    printf("Topper: %s with total %d\n", studentNames[topperIndex], highestTotal);
+    printf("-----------------------------------------\n");
+
+    return 0;
+}
