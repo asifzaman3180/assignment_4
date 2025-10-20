39b365078ed53f5b10a6aefdb1de1a745b625a66
Rahat2410 <s2410676133@ru.ac.bd>
2025-10-20 13:13:58 +0600

    Rahat Hossain id: 2410676133

--- myname.c
+++ myname.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Your Name Here\n"); // Replace "Your Name Here" with your actual name
+    printf(" I am Rahat Hossain"); // Replace "Your Name Here" with your actual name
     return 0;
 }
