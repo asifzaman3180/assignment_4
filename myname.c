7d06588a9b85f796068259d439ebbd14e27d82d8
Debjani <debjanibose2023@gmail.com>
2025-10-20 11:36:04 +0600

    Added student info: Debjani Bose(2412576127)

--- myname.c
+++ myname.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Your Name Here\n"); // Replace "Your Name Here" with your actual name
+    printf("Debjani Bose\n"); // Replace "Your Name Here" with your actual name
+    printf("2412576127");
     return 0;
 }
