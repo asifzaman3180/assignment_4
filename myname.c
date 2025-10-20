7027d31bf82adc5be9ba1410b4fd2a0d9f243200
milon <milonhosen@gmail.com>
2025-10-20 17:38:01 +0600

    Added student info: milon (2410476141)

--- myname.c
+++ myname.c
@@ -2,5 +2,7 @@
 
 int main() {
     printf("Your Name Here\n"); // Replace "Your Name Here" with your actual name
+    printf("student name : milon");
+    printf("studen id:2410476141");
     return 0;
 }
