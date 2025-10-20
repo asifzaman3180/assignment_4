8c245f6fb6b436388412840a484f2341b2bc32e0
tousif-mahir <almahir457@gmail.com>
2025-10-20 12:35:26 +0600

    Added Student info and clean code

--- cleancode.c
+++ cleancode.c
@@ -1,3 +1,52 @@
 #include<stdio.h>
-int main(){int a[3][3]={{50,60,70},{80,90,100},{30,40,50}};char n[3][10]={"Ali","Bob","Cat"};int i,j,s;float avg;
-for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}avg=s/3.0;if(avg>=80)printf("%s Grade A avg=%.2f\n",n[i],avg);else if(avg>=60)printf("%s Grade B avg=%.2f\n",n[i],avg);else if(avg>=40)printf("%s Grade C avg=%.2f\n",n[i],avg);else printf("%s Fail avg=%.2f\n",n[i],avg);}int max=0,pos=0;for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}if(s>max){max=s;pos=i;}}printf("Topper: %s with total %d\n",n[pos],max);return 0;}
+void average(); //function decleration
+void grade(); //function decleration
+int main()
+{
+    void average();  //function call
+    void grade();  //function call
+    return 0;
+}
+void average()  //function definition
+{
+    int mark[3][3]={{50,60,70},{80,90,100},{30,40,50}}; //naming variables
+    char name[3][10]={"Ali","Bob","Cat"}; //naming variables
+    int i,j,sum; //naming variables
+    float avg;
+    for(i=0;i<3;i++){
+        sum=0;
+        for(j=0;j<3;j++){
+            sum+=mark[i][j];
+        }
+        avg=sum/3.0;
+        if(avg>=80){
+            printf("%s Grade A avg=%.2f\n",name[i],avg);
+        }
+        else if(avg>=60){
+            printf("%s Grade B avg=%.2f\n",name[i],avg);
+        }
+        else if(avg>=40){
+            printf("%s Grade C avg=%.2f\n",name[i],avg);
+        } 
+        else{
+            printf("%s Fail avg=%.2f\n",name[i],avg);
+        }
+    }
+}
+void grade()  //function definition
+{
+    int mark[3][3]={{50,60,70},{80,90,100},{30,40,50}}; //naming variables
+    char name[3][10]={"Ali","Bob","Cat"}; //naming variables
+    int i, j, max=0,pos=0, sum=0; //naming variables
+    for(i=0;i<3;i++){
+        sum=0;
+        for(j=0;j<3;j++){
+            sum+=mark[i][j];
+        }
+        if(sum>max){
+            max=sum;
+            pos=i;
+        }
+    }
+    printf("Topper: %s with total %d\n",name[pos],max); //show the final result
+}
--- myname.c
+++ myname.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Your Name Here\n"); // Replace "Your Name Here" with your actual name
+    printf("Name:Md. Tousif Al MAhir\n");
+    printf("Student Id:2410476113");
     return 0;
 }
