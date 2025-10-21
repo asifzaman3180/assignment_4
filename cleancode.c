06e1db6d27538a10a10368f82c3b57c4c7133fdb
Nubah <nubahmahjabin@gmail.com>
2025-10-21 20:43:30 +0600

    Final code

--- cleancode.c
+++ cleancode.c
@@ -1,9 +1,49 @@
+#include<stdio.h> 
 
-int main(){int a[3][3]={{50,60,70},{80,90,100},{30,40,50}};char
-n[3][10]={"Ali","Bob","Cat"};int i,j,s;float avg;
-for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}avg=s/3.0;if(avg>=80)p
- rintf("%s Grade A avg=%.2f\n",n[i],avg);else if(avg>=60)printf("%s Grade
-B avg=%.2f\n",n[i],avg);else if(avg>=40)printf("%s Grade C
-avg=%.2f\n",n[i],avg);else printf("%s Fail avg=%.2f\n",n[i],avg);}int
-max=0,pos=0;for(i=0;i<3;i++){s=0;for(j=0;j<3;j++){s+=a[i][j];}if(s>max){
- max=s;pos=i;}}printf("Topper: %s with total %d\n",n[pos],max);return 0;}
+//Function to calculate average
+float average(int sum){
+    return sum/3.0;
+}
+
+//Function to calculate grade based on average
+void Grade_calculation(float avg, char name[]){
+    if(avg>=80)
+        printf("%s Grade A avg=%.2f\n",name,avg);
+    else if(avg>=60)
+        printf("%s Grade B avg=%.2f\n",name,avg);
+    else if(avg>=40)
+        printf("%s Grade C avg=%.2f\n",name,avg);
+    else 
+        printf("%s Fail avg=%.2f\n",name,avg);
+}
+
+int main(){
+    int marks[3][3]={{50,60,70},{80,90,100},{30,40,50}};
+    char names[3][10]={"Ali","Bob","Cat"};
+    int student,subject,sum;
+    float avg; 
+
+    //Calculate average and grade for each student
+    for(student=0;student<3;student++){
+        sum=0;
+        for(subject=0;subject<3;subject++){
+            sum+=marks[student][subject];
+        }
+            avg=average(sum);
+            Grade_calculation(avg,names[student]);
+    }
+        int max=0,pos=0;
+    
+        //Find the maximum scorer
+        for(student=0;student<3;student++){
+            sum=0;
+             for(subject=0;subject<3;subject++){
+                sum+=marks[student][subject];
+            }
+            if(sum>max){
+                max=sum;pos=student;
+            }
+        }
+        printf("Topper: %s with total %d\n",names[pos],max);
+ return 0;
+} 
